fc878e347c349b842f4eabc308c0431703f6a96f
PHP215 <christopherrobbins031@gmail.com>
2021-09-12 18:18:42 +0100

    setting up the login system

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,25 @@
 	  <div class="container">
 		  <div class="row justify-content-center align-items-center">
 			  <div class="col-md-4 shadow p-2">
-				  <h2 class="text-secondary">Login System</h2>
-				  <div class="form-group my-2">
-					<label for="">Username</label>
-					<input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
-				</div>
-				<div class="form-group my-2">
-					<label for="">Password</label>
-					<input type="password" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
-					<button type="submit" class="my-2 btn btn-primary">Login</button>
+				    <form @submit.prevent="login">
+						<h2 class="text-secondary">Login System</h2>
+						<div class="form-group my-2">
+							<label for="">Username</label>
+							<input type="text" v-model="inputUsername" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+						</div>
+						<div class="form-group my-2">
+							<label for="">Password</label>
+							<input type="password" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+							<button type="submit" class="my-2 btn btn-primary">Login</button>
+						</div>
+					</form>
 			  </div>
+		  </div>
+		  <div class="row">
+			  <div class="col-md-6">
+				  <div class="chat view">
+					  <h1>Chat View</h1>
+				  </div>
 			  </div>
 		  </div>
 	  </div>
@@ -22,11 +31,34 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-
+import {reactive, onMounted, ref} from 'vue'
+import db from '../db'
 export default {
   name: 'Home',
   components: {
 
+  },
+
+  setup(){
+	const InputUsername = ref("");
+	const state = reactive({
+		username : "",
+		messages : []
+	});
+
+	const Login = ()=>{
+		if (inputUsername.value !== null || inputUsername.value !== "") {
+			state.username = inputUsername.value
+			inputUsername.value = ""
+		}
+	}
+
+
+	return {
+		InputUsername,
+		Login,
+		state
+	}
   }
 }
 </script>
