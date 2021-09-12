067f65a7c0e70e800e9fd7ebaa33fceb78da01eb
PHP215 <christopherrobbins031@gmail.com>
2021-09-12 17:22:16 +0100

    modified the package

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	  <div class="container">
+		  <div class="row justify-content-center align-items-center">
+			  <div class="form-group">
+				<label for="">Username</label>
+				<input type="text" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+			  </div>
+			  <div class="form-group">
+				<label for="">Password</label>
+				<input type="password" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
+			  </div>
+			  
+		  </div>
+	  </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+
   }
 }
 </script>
