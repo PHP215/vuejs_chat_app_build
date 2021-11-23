f4b703a03da8fb529ed2b397df19072baef29c8d
PHP215 <christopherrobbins031@gmail.com>
2021-11-23 22:20:15 +0000

    modified the register component

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,8 @@
 <template>
   <div id="nav">
-   <div class="container">
-		  <div class="row mt-5 justify-content-center align-items-center">
-			  <div class="col-md-4 shadow p-2">
-				    <form @submit.prevent="Register">
-              <h2 class="text-secondary text-center">Register</h2>
-              <!-- error message -->
-              <div class="error-message" v-if="register_state.errorMessage !== ''" :class="(register_state.errorMessage !== '' ? 'alert alert-danger' : '')">
-                {{register_state.errorMessage}}</div>
-                <!-- success message -->
-                <div class="success-message" v-if="register_state.successMessage !== ''" :class="(register_state.successMessage !== '' ? 'alert alert-success' : '')">
-                {{register_state.successMessage}}</div>
-              <div class="form-group my-2">
-                <label for="">Email</label>
-                <input type="email"  v-model="email" name="" id="" class="form-control" placeholder="" aria-describedby="helpId">
-              </div>
-              <div class="form-group my-2">
-                <label for="">Password</label>
-                <input type="password"  name="" v-model="password" id="" class="form-control" placeholder="" aria-describedby="helpId">
-              </div>
-              <div class="form-group my-2">
-                <label for="">Confirm Password</label>
-                <input type="password"  name="" v-model="c_password" id="" class="form-control" placeholder="" aria-describedby="helpId">
-              </div>
-               <button type="submit" class="my-2 btn btn-primary">Register</button>
-					</form>
-			  </div>
-		  </div>
-	</div>
+   
   </div>
-  <!-- <router-view/> -->
+  <router-view/>
 </template>
   <script>
     import {reactive, onMounted, ref} from 'vue'
@@ -46,12 +19,6 @@
         successMessage : ""
       })
       
-      // let errorMessage = '';
-      // const confirmPassword = ()=>{
-      //   if (this.password !== this.c_password) {
-      //     alert('nop')
-      //   }
-      // }
      const Register = ()=>{
       //  alert(password.value)
         if(password.value == '' || c_password.value == '' ||  email.value == ''){
@@ -68,7 +35,7 @@
           try {
             //  db.auth().createUserWithEmailAndPassword(email.value,password.value);
              register_state.successMessage = 'Registered Successfully!'
-             $redirect('/home')
+             window.location.href = '/views/Home'
                 console.log(register_state.successMessage)
           } catch (err) {
             console.log('error')
