a017d3b1d251f974a609f9fba698c8229fe71a1d
PHP215 <christopherrobbins031@gmail.com>
2021-12-04 05:31:28 +0100

    update homw view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -84,8 +84,28 @@ export default {
 		if (inputEmail.value !== '' && inputPassword.value !== "") {
       db.auth().signInWithEmailAndPassword(inputEmail.value,inputPassword.value).
       then((userCredential)=>{
-        state.username = inputEmail.value
-        inputEmail.value = ''
+        console.log(userCredential)
+      // const user_data = db.database().ref('users/' + userCredential.uid);
+      //      user_data.on('value', (snapshot) => {
+      //           const data_fetched = snapshot.val();
+      //           // this.handleData(data);
+      //           console.log(data_fetched)
+      //       });
+            db.auth().onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    var uid = user.uid;
+    console.log(uid)
+    // ...
+  } else {
+    // User is signed out
+    console.log('fuck')
+    // ...
+  }
+});
+        //   state.username = inputEmail.value
+        // inputEmail.value = ''
       }).
       catch((error)=>{
         state.error = error.message
