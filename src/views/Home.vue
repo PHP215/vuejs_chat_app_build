3e251976de49fbbc1b067c3ec25d58efc1f4a218
PHP215 <christopherrobbins031@gmail.com>
2021-12-04 20:50:54 +0100

    modified messaging system

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -39,8 +39,8 @@
 						<div class="chat" v-for="message in state.messages" :key="message.key" :class="message.username == state.username ? 'outgoing' : 'incoming'">
 							<div class="details">
 								<p>{{message.content}}</p>
-                <small class="text-secondary"><i class="fas fa-clock text-primary"></i> {{message.time}} </small>
-                <small v-if="state.email !== message.email && message.username !== message.username">{{message.username}}</small>
+                <small class="text-secondary"><i class="fas fa-clock text-primary"></i> {{message.time}} </small><br>
+                <small v-if="state.email !== message.email && state.username !== message.username"> <span>{{ message.username}}</span></small>
 							</div>
 						</div>
 						
